e0fab159aa91f9717908a03dd413f7ce1710b25d
Talha <talha43gs@gmail.com>
2024-03-20 20:34:43 +0000

    BaseProject Created

---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("TEST1");
+        System.out.println("TEST11");
+        System.out.println("TEST1111");
+    }
+}
