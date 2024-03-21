160593a6a7fd262252eed3fffdd7d4608f9fda6d
Talha <talha43gs@gmail.com>
2024-03-21 19:31:05 +0000

    Test1 updated

--- src/Test1.java
+++ src/Test1.java
@@ -1,8 +1,8 @@
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("TEST1");
-        System.out.println("TEST11");
+        System.out.println("local test 1");
+        System.out.println("local test 1111");
         System.out.println("TEST1111");
         System.out.println("TEST1111");
         System.out.println("TEST1111");
