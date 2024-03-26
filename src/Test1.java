0bb1b8bb4337b891a3a070d82da089916a8a6bef
Talha <talha43gs@gmail.com>
2024-03-26 18:32:51 +0000

    PR review fix on test1 file

--- src/Test1.java
+++ src/Test1.java
@@ -2,6 +2,7 @@ public class Test1 {
 
     public static void main(String[] args) {
 
+        System.out.println("local test 1");
         System.out.println("local test 1111");
         System.out.println("TEST11111111");
         System.out.println("TEST1111111");
